ffba02f40e253765b77fbc82674996d982f3e9be
ShaniaSanjana <shaniasanjana@gmail.com>
2022-07-08 17:45:45 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const XylophoneApp());
+}
+
+
+class XylophoneApp extends StatelessWidget {
+
+   void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('assets_note$soundNumber.wav');
+  }
+  Expanded buildKey( {required int soundNumber ,required Color color} ) {
+   return Expanded(
+       child: TextButton(
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        style: TextButton.styleFrom(
+          backgroundColor: color ,
+        ),
+        child: Container(),
+      ),
+    );
+  }
+
+  const XylophoneApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return (MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(soundNumber: 1,color: Colors.red),
+              buildKey(soundNumber: 2,color: Colors.orange),
+              buildKey(soundNumber: 3,color: Colors.yellow),
+              buildKey(soundNumber: 4,color: Colors.green),
+              buildKey(soundNumber: 5,color: Colors.teal),
+              buildKey(soundNumber: 6,color: Colors.blue),
+              buildKey(soundNumber: 7,color: Colors.purple),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+}
